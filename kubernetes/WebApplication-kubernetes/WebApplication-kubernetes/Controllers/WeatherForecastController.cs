d5053badce2753d77ee72f44fdab973e51fe7f39
Roberto Camara <robertocamara@outlook.com.br>
2021-12-30 18:51:35 -0300

    kubernetes metrics and autoscale

--- kubernetes/WebApplication-kubernetes/WebApplication-kubernetes/Controllers/WeatherForecastController.cs
+++ kubernetes/WebApplication-kubernetes/WebApplication-kubernetes/Controllers/WeatherForecastController.cs
@@ -18,22 +18,89 @@ namespace WebApplication_kubernetes.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        [HttpGet("hello")]
-        public string Hello()
-        {
-            return $"Hello World {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}";
-        }
+        private static DateTime _startApp = DateTime.Now;
+        private static Random _rnd = new();
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
         }
 
+        [HttpGet("health")]
+        public IActionResult Health()
+        {
+            var duration = DateTime.Now.Subtract(_startApp).TotalSeconds;
+
+            if (duration < 10)
+            {
+                _logger.LogError("application is not ready");
+                throw new Exception("application is not ready");
+            }
+
+            return Ok($"App is running at: {duration} seconds");
+        }
+
+        [HttpGet("hello")]
+        public string Hello()
+        {
+            string name = Environment.GetEnvironmentVariable("MYNAME") ?? "N/A";
+
+            return $"Welcome {name}, {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}";
+        }
+
+        [HttpGet("generate-guid")]
+        public IEnumerable<string> GenerateGuids()
+        {
+            try
+            {
+                var delay = new[]
+                {
+                    true, false
+                };
+
+                var useDelay = delay[_rnd.Next(delay.Length)];
+                if (useDelay)
+                    Task.Delay(TimeSpan.FromSeconds(1)).GetAwaiter().GetResult();
+
+                return Enumerable.Range(1, 100)
+                        .Select(s => Guid.NewGuid().ToString())
+                        .ToArray();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return null;
+            }
+        }
+
+        [HttpGet("skills")]
+        public async Task<IActionResult> ReadFileAsync()
+        {
+            try
+            {
+                var skills = await System.IO.File.ReadAllTextAsync(@"staticfiles/skills.txt");
+                return Ok(skills);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error reading file: {ex.Message}");
+            }
+        }
+
+        [HttpGet("secret")]
+        public async Task<IActionResult> Secret()
+        {
+            var username = Environment.GetEnvironmentVariable("USERNAME") ?? "USERNAME";
+            var password = Environment.GetEnvironmentVariable("PASSWORD") ?? "PASSWORD";
+
+            return Ok($"Username: {username}, Password: {password}");
+        }
+
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 100).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
