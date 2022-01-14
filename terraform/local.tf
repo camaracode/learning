6ea288ec1a6664a42cc1405223379d7535100c99
Roberto Camara <robertocamara@outlook.com.br>
2022-01-14 11:29:28 -0300

    criando kubeconfig, instalando aws-iam-authenticator e testando deploy

--- terraform/local.tf
+++
@@ -1,9 +0,0 @@
-# resource = bloco
-# local = provider
-# file = tipo do provider
-# exemplo = nome do recurdo
-resource "local_file" "exemplo" {
-  filename = "exemplo.txt"
-  content = "Roberto Camara - Terraform"
-}
-
